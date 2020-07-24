9242decc90922fcacaf4e00853405143cea2b38a
Sébastien DUPAS <se.dupas@orange.fr>
2020-07-24 12:29:27 +0200

    Initial commit

---
+++ Addition.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Addition
+{
+    public Addition(int a, int b)
+    {
+        Resultat = a + b;
+    }
+
+    public long Resultat { get; }
+}
---
+++ AdditionTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Angband.Core.Test
+{
+    [TestCategory("Unit")]
+    [TestClass]
+    public class AdditionTest
+    {
+        [TestMethod]
+        public void Zero_Plus_Zero_Font_Zero()
+        {
+            var addition = new Addition(0, 0);
+            Assert.AreEqual(0, addition.Resultat);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 6, 7)]
+        [DataRow(31, -6, 25)]
+        public void X_Plus_Y_Font_La_Somme_Des_Deux(int x, int y, int resultat)
+        {
+            var addition = new Addition(x, y);
+            Assert.AreEqual(resultat, addition.Resultat);
+        }
+    }
+}
